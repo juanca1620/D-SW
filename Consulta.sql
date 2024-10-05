6959fe059997f5b5d131570c50a0767bcd2e4265
Juan Camilo Soto <juanca162072@gmail.com>
2024-10-05 04:11:42 -0500

    updated the SQL skeleton

--- Consulta.sql
+++ Consulta.sql
@@ -11,11 +11,11 @@ INSERT INTO roles (id,nombre)VALUES
 (1,"AdminGeneral"),
 (2,"Cliente");
 
+
 CREATE TABLE usuarios(
   id INT PRIMARY KEY,
   nombre VARCHAR(100),
   fecha_nacimiento DATE,
-  gmail VARCHAR(100),
   contrasenna VARCHAR(255),
   id_rol INT,
   FOREIGN KEY (id_rol) REFERENCES roles(id)
@@ -43,16 +43,34 @@ CREATE TABLE produccion(
 CREATE TABLE reportes(
   id INT AUTO_INCREMENT PRIMARY KEY,
   argumento TEXT,
-  id_produccion INT,
-  FOREIGN KEY (id_produccion) REFERENCES lotes(id)
+  tipo_reporte INT,
+  id_produccion INT ,
+  id_lote INT ,
+  FOREIGN KEY (id_produccion) REFERENCES produccion(id),
+  FOREIGN KEY (id_lote) REFERENCES lotes(id),
+  CHECK(
+    (id_lote IS NOT NULL AND id_produccion IS NULL) OR
+    (id_lote IS NULL AND id_produccion IS NOT NULL)
+  )
 )
-
 
 CREATE TABLE lotes(
   id INT PRIMARY KEY,
   nombre VARCHAR(100),
   nombre_ubicacion VARCHAR(100)
 )
+
+INSERT INTO lotes (id, nombre, nombre_ubicacion) VALUES
+(1, 'Lote 1', 'Ubicación 1'),
+(2, 'Lote 2', 'Ubicación 2'),
+(3, 'Lote 3', 'Ubicación 3'),
+(4, 'Lote 4', 'Ubicación 4'),
+(5, 'Lote 5', 'Ubicación 5'),
+(6, 'Lote 6', 'Ubicación 6'),
+(7, 'Lote 7', 'Ubicación 7'),
+(8, 'Lote 8', 'Ubicación 8'),
+(9, 'Lote 9', 'Ubicación 9'),
+(10, 'Lote 10', 'Ubicación 10');
 
 CREATE TABLE gallinas(
   id INT PRIMARY KEY,
@@ -61,12 +79,13 @@ CREATE TABLE gallinas(
   id_lote INT,
   FOREIGN KEY (id_lote) REFERENCES lotes(id)
 )
+
 CREATE TABLE alimentacion(
   id INT AUTO_INCREMENT PRIMARY KEY,
   tipo_alimento VARCHAR(30),
-  cantidad_alimento 
+  cantidad_alimento INT,
   fecha_alimento DATE,
   id_lote INT,
   FOREIGN KEY (id_lote) REFERENCES lotes (id)
 )
-  
+
